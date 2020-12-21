1c9563219fd06ae9c340c7f6a41b48aee9f1fe4a
nandini <nandinisharma6th@gmail.com>
2020-12-21 15:05:47 +0530

    k

--- Demo.java
+++ Demo.java
@@ -3,14 +3,8 @@ public class Demo {
 	
 	public static void main(String[] args) {
 		
-		int a= 12;
-		int b = 15;
 		
-		System.out.println("a value = "+a+"b value is = "+b);
+		System.out.println("hello");
 		
-		a= a+b;
-		b= a-b;
-		a= a-b;
-		
-		System.out.println("Values after swap a= "+a+"b= "+b );
 	}
+}
